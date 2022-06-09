d83affc2c20da1596a56d7b46750f19784765038
Yohan Kim <ddalpange@gmail.com>
2022-06-09 18:14:35 +0900

    fix onlyNamedExport that wrties wrong codes

--- www-snippet/create-react-component/createReactComponentContent.ts
+++ www-snippet/create-react-component/createReactComponentContent.ts
@@ -6,36 +6,37 @@ export const createReactComponentContent = (
 ) => {
   if (!option.forwardRef) {
     return `import React, { memo, PropsWithChildren } from "react";
-export interface ${name}Props {}
 
-const ${name} = (({}: PropsWithChildren<${name}Props>) => {
-  return <>${name}</>;
-});
+export interface ${name}Props {}
 
 ${
       option.onlyNamedExport
-        ? `export { ${name}: memo(${name}) }`
-        : `export default memo(${name})`
-    };
+        ? "export const"
+        : "const"
+    } ${name} = memo((({}: PropsWithChildren<${name}Props>) => {
+  return <>${name}</>;
+}));
+
+${option.onlyNamedExport === false ? `export default ${name};` : ""}
 `;
   }
 
-  return `import React, { forwardRef, memo } from "react";
+  return `import React, { forwardRef, memo, HTMLAttributes } from "react";
 
 type RefElement = HTMLDivElement;
-type RefElementProps = React.HTMLAttributes<HTMLDivElement>;
+type RefElementProps = HTMLAttributes<HTMLDivElement>;
 
 export interface ${name}Props extends RefElementProps {}
 
-const ${name} = forwardRef<RefElement, ${name}Props>((props, ref) => {
-  return <div ref={ref} {...props}>Lorem Ipsum</div>;
-});
-
 ${
     option.onlyNamedExport
-      ? `export { ${name}: memo(${name}) }`
-      : `export default memo(${name})`
-  }
+      ? "export const"
+      : "const"
+  } ${name} = memo(forwardRef<RefElement, ${name}Props>((props, ref) => {
+  return <div ref={ref} {...props}>Lorem Ipsum</div>;
+}));
+
+${option.onlyNamedExport === false ? `export default ${name};` : ""}
 
 ${name}.displayName = "${name}";
 `;
